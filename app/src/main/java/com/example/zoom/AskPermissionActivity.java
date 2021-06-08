069abe3d2c92babaedbf8bcfcaec1083dfcbe505
Eduardo Graça <e.graca@campus.fct.unl.pt>
2021-06-08 16:54:56 +0100

    Improved Request Join Meeting pop up

--- app/src/main/java/com/example/zoom/AskPermissionActivity.java
+++ app/src/main/java/com/example/zoom/AskPermissionActivity.java
@@ -6,14 +6,43 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
 import android.view.Gravity;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class AskPermissionActivity extends Activity {
+
+    Button acceptButton;
+    Button rejectButton;
+    TextView textBox;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ask_permission);
+
+        acceptButton = (Button) findViewById(R.id.acceptButton);
+        rejectButton = (Button) findViewById(R.id.rejectButton);
+        textBox = (TextView) findViewById(R.id.textBox);
+        textBox.setGravity(Gravity.CENTER_HORIZONTAL);
+
+        textBox.append("Participant requesting entry!");
+
+        acceptButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+        rejectButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
 
         DisplayMetrics displayMetrics = new DisplayMetrics();
         getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
@@ -21,7 +50,7 @@ public class AskPermissionActivity extends Activity {
         int width = displayMetrics.widthPixels;
         int height = displayMetrics.heightPixels;
 
-        getWindow().setLayout((int)(width*.8), (int)(height*.5));
+        getWindow().setLayout((int)(width*.8), (int)(height*.3));
 
         WindowManager.LayoutParams params = getWindow().getAttributes();
         params.gravity = Gravity.CENTER;
