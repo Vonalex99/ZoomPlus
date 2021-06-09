308cc8b410d9d8373d7b1b886729d03183dca13b
Eduardo Graça <e.graca@campus.fct.unl.pt>
2021-06-09 18:31:58 +0100

    Removed trash text box

--- app/src/main/java/com/example/zoom/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/zoom/ui/home/HomeFragment.java
@@ -38,14 +38,6 @@ public class HomeFragment extends Fragment {
         binding = FragmentHomeBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textHome;
-        homeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
-
         scheduleButton = binding.scheduleButton;
         scheduleButton.setOnClickListener(new View.OnClickListener() {
             @Override
